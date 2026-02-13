7b12f1c3bbaa3437fe3057c5548375d7d6cce537
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2026-02-13 12:35:58 +0000

    add react Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tooltip from './Tooltip';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h2>Tooltip Demo</h2>
+
+      <p>
+        Hover over the <Tooltip text="I am a tooltip for the word 'word'">word</Tooltip> to see the tooltip.
+      </p>
+
+      <p>
+        Here's a <Tooltip text="Tooltip on a button"><button>button</button></Tooltip> with a tooltip.
+      </p>
+
+      <p>
+        Emoji: <Tooltip text="Smiley tooltip">🙂</Tooltip>
+      </p>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
