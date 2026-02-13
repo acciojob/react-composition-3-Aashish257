0ac7d5f214b70e5ac08d7755ccc8b599046ce518
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2026-02-13 12:59:02 +0000

    update v4

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,6 @@ const App = () => {
   return (
     <div>
       {/* Do not remove the main div */}
-      <h2>Tooltip Demo</h2>
 
       <p>
         <Tooltip text="This is another tooltip">Hover over me to see another tooltip</Tooltip>
